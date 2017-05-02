c6019a7256ae21e4678f84a49a1535739084625d
Steven DUMONT <windir10@gmail.com>
2017-05-02 15:12:44 +0200

    config app extension twig + create admin view

--- app/app.php
+++ app/app.php
@@ -21,9 +21,16 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 
 // Register service providers
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app['twig'] = $app->extend('twig', function(Twig_Environment $twig, $app) {
+    $twig->addExtension(new Twig_Extensions_Extension_Text());
+    return $twig;
+});
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
 $app->register(new Silex\Provider\AssetServiceProvider(), array(
     'assets.version' => 'v1'
 ));
