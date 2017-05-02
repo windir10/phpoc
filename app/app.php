577327b64dfe8e8e89e90bba93bb694cd00d4404
Steven DUMONT <windir10@gmail.com>
2017-05-02 21:56:38 +0200

    add log system

--- app/app.php
+++ app/app.php
@@ -53,6 +53,12 @@ $app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\LocaleServiceProvider());
 $app->register(new Silex\Provider\TranslationServiceProvider());
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/microcms.log',
+    'monolog.name' => 'MicroCMS',
+    'monolog.level' => $app['monolog.level']
+));
+
 // Register services
 $app['dao.article'] = function ($app) {
     return new MicroCMS\DAO\ArticleDAO($app['db']);
